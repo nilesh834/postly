b913cc073132566385aabb539d8ef95fe31c44d0
nilesh <nilesh6174@gmail.com>
2026-05-19 21:32:44 +0530

    4.2

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,14 +5,12 @@ import { error, success } from "../utils/responseWrapper.js";
 
 const createAccessToken = (payload) =>
   jwt.sign(payload, process.env.JWT_SECRET, {
-    // expiresIn: "15m",
-    expiresIn: "1m",
+    expiresIn: "15m",
   });
 
 const createRefreshToken = (payload) =>
   jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
-    // expiresIn: "7d",
-    expiresIn: "5m",
+    expiresIn: "7d",
   });
 
 export const signupController = async (req, res) => {
@@ -72,8 +70,7 @@ export const loginController = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      // maxAge: 15 * 60 * 1000, // 15 mins
-      maxAge: 60 * 1000, // 1 minute for testing
+      maxAge: 15 * 60 * 1000, // 15 mins
       path: "/",
     });
 
@@ -82,8 +79,7 @@ export const loginController = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      // maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      maxAge: 5 * 60 * 1000, // 5 minutes for testing
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       path: "/",
     });
 
